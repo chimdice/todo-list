ae181457ece74fe87e16b71c2989157a885d4d42
chimdice <118031439+chimdice@users.noreply.github.com>
2024-06-13 18:40:08 -0700

    Add project deletion logic

--- src/index.js
+++ src/index.js
@@ -1,23 +1,51 @@
-import { CreateProjectOnPage, LoadProjectOnPage, LoadProjectCreation } from "./load.js";
+import { CreateProjectOnPage, LoadProjectOnPage, LoadProjectCreation, LoadProjectDeletion, clearPage} from "./load.js";
 import { todoLists } from "./todo.js";
 import "./style.css";
 
 const todosSection = document.querySelector(".sidebar-content");
 const addProject = document.querySelector(".add-project");
-const main = document.querySelector('.main-section')
+const main = document.querySelector('.main-section');
+const deleteButton = document.querySelector(".delete-project");
 
 const mainLists = new todoLists(todosSection);
 
 addProject.addEventListener("click", () => {
     const submit = LoadProjectCreation(main)
     submit.addEventListener("click", (event) => {
-        const projectName = document.querySelector("#project-label").value;
-        mainLists.addProject(projectName);
-        const projectSideBar = CreateProjectOnPage(projectName);
+        const projectName = document.querySelector("#project-label");
+        const projectNameValue = projectName.value
+        mainLists.addProject(projectNameValue);
+        const projectSideBar = CreateProjectOnPage(projectNameValue);
         LoadProjectOnPage(todosSection, projectSideBar)
+        projectName.value = ''
         console.log(mainLists.projects);
         event.preventDefault()
     })
 })
+
+deleteButton.addEventListener("click", () => {
+    const submit = LoadProjectDeletion(main, mainLists.projects);
+
+    submit.addEventListener('click', (event) => {
+        const options = document.querySelectorAll('#project-check');
+        const checkedProjects = [];
+        options.forEach(option => {
+            if (option.checked) {
+                const label = option.nextElementSibling;
+                checkedProjects.push(label.textContent);
+            };
+        });
+
+        for (let key in checkedProjects) {
+            const id = document.querySelector(`${key}-list`)
+            todosSection.removeChild(id);
+            mainLists.deleteProject(key);
+        };
+
+        clearPage(main);
+        console.log(mainLists.projects);
+        event.preventDefault();
+    });
+});
 
 console.log(mainLists.projects);
--- src/load.js
+++ src/load.js
@@ -1,7 +1,16 @@
+const clearPage = (main) => {
+    while (main.firstChild) {
+        main.removeChild(main.lastChild)
+    };
+};
+
+/*Create Project */
+
 const CreateProjectOnPage = function (name) {
     const projectElement = document.createElement('div');
-    projectElement.id = `${name}-list`
+    projectElement.id = `${name}-list`;
     projectElement.textContent = name;
+    
     return projectElement;
 };
 
@@ -14,7 +23,7 @@ const LoadProjectFourm = () => {
 
     const label = document.createElement('label');
     label.setAttribute('for', 'project-label');
-    label.textContent = "Project Name:"
+    label.textContent = "Project Name:";
 
     const input = document.createElement("input");
     input.setAttribute('type', 'text');
@@ -34,9 +43,7 @@ const LoadProjectFourm = () => {
 };
 
 const LoadProjectCreation = (main) => {
-    while (main.firstChild) {
-        main.removeChild(main.lastChild)
-    };
+    clearPage(main)
 
     const header = document.createElement('h2');
     header.textContent = "Create New Project!"
@@ -46,4 +53,45 @@ const LoadProjectCreation = (main) => {
     main.appendChild(form)
     return form.lastChild
 };
-export {CreateProjectOnPage, LoadProjectOnPage, LoadProjectCreation}
+
+/*Delete Project */
+const LoadDeleteFourm = (todos) => {
+    const form = document.createElement('form');
+
+    for (let key in todos) {
+        const project = document.createElement('div');
+
+        const label = document.createElement('label');
+        label.setAttribute('for', `project-check`);
+        label.textContent = key;
+
+        const input = document.createElement("input");
+        input.setAttribute('type', 'checkbox');
+        input.setAttribute('name', `project-check`);
+        input.setAttribute('id', `project-check`);
+
+        project.appendChild(input);
+        project.appendChild(label);
+        form.appendChild(project);
+    };
+
+    const inputButton = document.createElement("button");
+    inputButton.setAttribute('type', 'submit');
+    inputButton.id = 'submit-button';
+    inputButton.textContent = 'Delete';
+
+    form.appendChild(inputButton);
+
+    return form
+};
+
+const LoadProjectDeletion = (main, todos) => {
+    clearPage(main);
+    const form = LoadDeleteFourm(todos);
+    const header = document.createElement('h2');
+    header.textContent = "Choose Project(s) to Delete";
+    main.appendChild(form);
+    return form.lastChild;
+};
+
+export {CreateProjectOnPage, LoadProjectOnPage, LoadProjectCreation, LoadProjectDeletion, clearPage}
